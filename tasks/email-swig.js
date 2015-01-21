ccd73d4ece469d9f294333f483d38cbb25677017
bmcminn <labs@gbox.name>
2015-01-21 01:08:28 -0600

    swig-email task is togethcoming together

--- tasks/email-swig.js
+++ tasks/email-swig.js
@@ -1,28 +1,62 @@
 "use strict";
 
-// SWIG DOCS: http://paularmstrong.github.io/swig/
-
-var _     = require('lodash')
-  , swig  = require('swig')
-  ;
-
-
 module.exports = function (grunt) {
+
+  var _     = require('lodash')
+    , swig  = require('swig')
+    ;
+
+
+  var paths = {
+        email: {
+          src:  process.cwd() + "/templates/email",
+          dest: process.cwd() + "/___email_test"
+        }
+      }
+    ;
+
+
+  // SWIG DOCS: http://paularmstrong.github.io/swig/
+  swig.setDefaults({
+    loader:   swig.loaders.fs(process.cwd() + '/templates' )
+  });
+
 
   grunt.registerTask('email-swig', 'Compiles email templates with Swig to.', function() {
 
-    // grunt.log.ok('pants');
+    var emails = grunt.file.expand({}, paths.email.src + '/*.hypr')
+      , template
+      ;
 
-    var template = swig.renderFile('./___swigTest/test.hypr', {
-      pagename: "Testing 'pagename'",
-      authors: [
-        "Testing Author1",
-        "Testing Author2",
-        "Testing Author3"
-      ]
+
+    _.each(emails, function(filePath) {
+
+      // ignore email.hypr because we're extending it already
+      if (filePath.match(/email.hypr/)) { return; }
+
+      grunt.log.writeln(filePath);
+
+      template = swig.renderFile(filePath, {});
+
+      grunt.log.writeln(template);
+
     });
 
-    grunt.log.writeln(template);
+
+    // grunt.log.writeln(JSON.stringify(emails));
+
+    // var template = swig.renderFile('./___swigTest/test.hypr', {
+    //   pagename: "Testing 'pagename'",
+    //   authors: [
+    //     "Testing Author1",
+    //     "Testing Author2",
+    //     "Testing Author3"
+    //   ]
+    // });
+
+
+
+    // grunt.log.writeln(template);
 
   });
 
